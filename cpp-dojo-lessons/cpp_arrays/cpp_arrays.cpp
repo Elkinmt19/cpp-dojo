a439fc331adab39a13d20a193aba3d3b79d025fc
Elkinmt19 <elkin02011@hotmail.com>
2021-05-17 17:01:31 -0500

    cpp-arrays lesson added

---
+++ cpp-dojo-lessons/cpp_arrays/cpp_arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /**
+     * This is a script to start working with arrays in c++
+     */
+
+    int luckyNums[] = {4, 8, 15, 17, 43}; // Crearing a known array of integers
+
+    cout << luckyNums[0] << endl; // Accessing to a specific element of the array
+
+    luckyNums[2] = 19; // Changing a specific element of the array
+
+    int unknowarray[10]; // Creating an unknown array
+
+    return 0;
+}
