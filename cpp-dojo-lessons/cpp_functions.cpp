a747cf307f3ec630a6d6d4eee92a1f6804fd94f6
Elkinmt19 <elkin02011@hotmail.com>
2021-05-21 19:58:59 -0500

    cpp_functions.cpp file created and vscode ignore added to the .gitignore file

---
+++ cpp-dojo-lessons/cpp_functions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// The functions are declared in order to use them and define them below the main()
+void sayHi(string name, int age);
+double cube(double num);
+
+int main()
+{
+    /**
+     * This is a script to start working with functions in c++
+     */
+
+    cout << "Top" << endl;
+    sayHi("Elkin", 21);
+    cout << "Button" << endl;
+
+    double answer = cube(5.0);
+
+    return 0;
+}
+
+// Function to say Hi to the user
+void sayHi(string name, int age)
+{
+    cout << "Hello User" << endl;
+    cout << "Hello " << name << "you are " << age;
+}
+
+// Function to calculate the cubic of a number
+double cube(double num)
+{
+    double result = num*num*num;
+    return result;
+}
